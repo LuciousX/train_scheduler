9e017232eadb4a2ae89692787e8895d2f2d52513
Lucious Jackson <luciousjackson49@gmail.com>
2019-04-27 23:36:35 -0700

    Train HW

---
+++ js/script.js
@@ -0,0 +1,130 @@
+// Initialize Firebase
+var index = 0;
+
+  var config = {
+    apiKey: "AIzaSyAVeB0qjj39tZLcwHF_QZb4KxHzMQPYAhA",
+    authDomain: "train-schedule-fb5df.firebaseapp.com",
+    databaseURL: "https://train-schedule-fb5df.firebaseio.com",
+    projectId: "train-schedule-fb5df",
+    storageBucket: "train-schedule-fb5df.appspot.com",
+    messagingSenderId: "487599711279"
+  };
+  firebase.initializeApp(config);
+
+
+
+var database = firebase.database();
+
+$("#formID").on("submit", function (event) {
+    event.preventDefault();
+
+    var name = $("#trainName").val().trim();
+    var destination = $("#trainDestination").val().trim();
+    var firstTime = $("#firstTrainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency
+    });
+
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#frequency").val("");
+
+    return false;
+  });
+
+database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
+
+  var updateButton = $("<button>").html("<span class='glyphicon glyphicon-edit'></span>").addClass("updateButton").attr("data-index", index).attr("data-key", snapshot.key);
+  var removeButton = $("<button>").html("<span class='glyphicon glyphicon-remove'></span>").addClass("removeButton").attr("data-index", index).attr("data-key", snapshot.key);
+
+  var firstTime = snapshot.val().firstTime;
+  var trnFrequency = parseInt(snapshot.val().frequency);
+  var firstTrain = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTrain);
+  console.log(firstTime);
+  var currentTime = moment();
+  var currentTimeCalc = moment().subtract(1, "years");
+  var diffTime = moment().diff(moment(firstTrain), "minutes");
+  var tRemainder = diffTime%trnFrequency;
+  var minutesRemaining = trnFrequency - tRemainder;
+  var nextTRain = moment().add(minutesRemaining, "minutes").format ("hh:mm A");
+  var beforeCalc = moment(firstTrain).diff(currentTimeCalc, "minutes");
+  var beforeMinutes = Math.ceil(moment.duration(beforeCalc).asMinutes());
+
+  if ((currentTimeCalc - firstTrain) < 0) {
+    nextTrain = snapshot.val().firstTime;
+    console.log("Before First Train");
+    minutesRemaining = beforeMinutes;
+  }
+  else {
+    nextTrain = moment().add(minutesRemaining, "minutes").format("hh:mm A");
+    minutesRemaining = trnFrequency - tRemainder;
+    console.log("Working");
+  }
+
+
+  var tabRow = $("<tr>");
+  tabRow.addClass("row-" + index);
+  var cell1 = $("<td>").append(updateButton);
+  var cell2 = $("<td>").text(snapshot.val().name);
+  var cell3 = $("<td>").text(snapshot.val().destination);
+  var cell4 = $("<td>").text(snapshot.val().frequency);
+  var cell5 = $("<td>").text(nextTrain);
+  var cell6 = $("<td>").text(minutesRemaining);
+  var cell7 = $("<td>").append(removeButton);
+
+  tabRow
+    .append(cell1)
+    .append(cell2)
+    .append(cell3)
+    .append(cell4)
+    .append(cell5)
+    .append(cell6)
+    .append(cell7);
+
+ $("#tableContent").append(tabRow);
+
+ index++;
+  
+}, function (error) {
+
+  alert(error.code);
+
+});
+
+function removeRow () {
+  $(".row-" + $(this).attr("data-index")).remove();
+  database.ref().child($(this).attr("data-key")).remove();
+};
+
+function editRow () {
+  $(".row-" + $(this).attr("data-index")).children().eq(1).html("<textarea class='newName'></textarea>");
+  $(".row-" + $(this).attr("data-index")).children().eq(2).html("<textarea class='newDestination'></textarea>");
+  $(".row-" + $(this).attr("data-index")).children().eq(3).html("<textarea class='newFrequency' type='number'></textarea>");
+  $(this).toggleClass("updateButton").toggleClass("submitButton");
+};
+
+function submitRow () {
+  var newName = $(".newName").val().trim();
+  var newDestination = $(".newDestination").val().trim();
+  var newFrequency = $(".newFrequency").val().trim();
+
+  database.ref().child($(this).attr("data-key")).child("name").set(newName);
+  database.ref().child($(this).attr("data-key")).child("destination").set(newDestination);
+  database.ref().child($(this).attr("data-key")).child("frequency").set(newFrequency);
+
+  $(".row-" + $(this).attr("data-index")).children().eq(1).html(newName);
+  $(".row-" + $(this).attr("data-index")).children().eq(2).html(newDestination);
+  $(".row-" + $(this).attr("data-index")).children().eq(3).html(newFrequency);
+  $(this).toggleClass("updateButton").toggleClass("submitButton");
+};
+
+$(document).on("click", ".removeButton", removeRow);
+$(document).on("click", ".updateButton", editRow);
+$(document).on("click", ".submitButton", submitRow);
